2d77c62a332aca4f4b1ded210f2d39e5a76bab9d
Trent Mick <trentm@gmail.com>
2025-09-11 21:33:11 -0700

    gitignore usage for eslint; add self license-header plugin; add prettier
    Now 'npm install' uses 4-space indent for package-lock.json. Which is
    suprising to me.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,34 @@
+/*
+ * Copyright Trent Mick
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {fileURLToPath} from 'node:url';
 import js from '@eslint/js';
+import {includeIgnoreFile} from '@eslint/compat';
 import globals from 'globals';
 import {defineConfig} from 'eslint/config';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import headerPlugin from './index.js';
+
+const gitignore = fileURLToPath(new URL('.gitignore', import.meta.url));
 
 export default defineConfig([
+    includeIgnoreFile(gitignore, 'Imported .gitignore patterns'),
     {
         files: ['**/*.{js,mjs,cjs}'],
-        plugins: {js},
+        plugins: {js, 'yet-another-license-header': headerPlugin},
         extends: ['js/recommended'],
         languageOptions: {globals: globals.browser},
+        rules: {
+            'yet-another-license-header/header': [
+                'error',
+                {
+                    headerFile: './etc/license-header.txt',
+                },
+            ],
+        },
     },
-    // TODO: grok this
     {files: ['**/*.js'], languageOptions: {sourceType: 'commonjs'}},
     eslintConfigPrettier,
 ]);
