3ee71a4aacbf5c52ed80f81fe7ee64fe8df22b4e
Trent Mick <trentm@gmail.com>
2025-09-12 11:01:18 -0700

    lint with eslint-plugin-eslint-plugin, per eslint docs rec

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,8 @@ import {includeIgnoreFile} from '@eslint/compat';
 import globals from 'globals';
 import {defineConfig} from 'eslint/config';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import eslintPlugin from 'eslint-plugin-eslint-plugin';
+
 import headerPlugin from './index.js';
 
 // https://eslint.org/docs/latest/use/configure/ignore#including-gitignore-files
@@ -16,6 +18,11 @@ const gitignore = fileURLToPath(new URL('.gitignore', import.meta.url));
 
 export default defineConfig([
     includeIgnoreFile(gitignore, 'Imported .gitignore patterns'),
+
+    // Per recommendation at https://eslint.org/docs/latest/extend/plugins#linting-a-plugin
+    // Note: Not using `eslint-plugin-n` because it chokes on this .mjs file.
+    eslintPlugin.configs.recommended,
+
     {
         files: ['**/*.{js,mjs,cjs}'],
         plugins: {js, 'yet-another-license-header': headerPlugin},
@@ -30,6 +37,10 @@ export default defineConfig([
             ],
         },
     },
+
+    // ESLint flat-config assumes .js files are ESM without this.
     {files: ['**/*.js'], languageOptions: {sourceType: 'commonjs'}},
+
+    // Turn off style-related rules, because we separately use prettier for that.
     eslintConfigPrettier,
 ]);
