710b46d08a64a6ec4b5d796438e7a4d9b54bb332
Trent Mick <trentm@gmail.com>
2025-09-11 21:12:06 -0700

    prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,11 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  // TODO: grok this
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  eslintConfigPrettier,
 ]);
